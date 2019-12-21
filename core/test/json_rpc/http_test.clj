843780d03ac09f6123c9855e03af3f113db702d3
Hindol Adhya <hiadhy@microsoft.com>
2019-12-22 02:01:50 +0530

    Also test for secure (over SSL) URLs

--- core/test/json_rpc/http_test.clj
+++ core/test/json_rpc/http_test.clj
@@ -7,17 +7,19 @@
 
 (deftest ^:integration clj-http-test
   (testing "POST requests"
-    (let [channel (client/open clj-http "https://postman-echo.com/post")]
-      (try
-        (let [request  {:jsonrpc "2.0"
-                        :method  "eth_blockNumber"
-                        :params  ["latest"]
-                        :id      1}
-              response (json/read-str (client/send! clj-http
-                                                    channel
-                                                    (json/write-str request))
-                                      :key-fn keyword)]
-          (is (= request (:json response)))
-          (is (= "application/json" (-> response :headers :content-type)))
-          (is (= "application/json" (-> response :headers :accept))))
-        (finally (client/close clj-http channel))))))
+    (doseq [url ["http://postman-echo.com/post"
+                 "https://postman-echo.com/post"]]
+      (let [channel (client/open clj-http url)]
+        (try
+          (let [request  {:jsonrpc "2.0"
+                          :method  "eth_blockNumber"
+                          :params  ["latest"]
+                          :id      1}
+                response (json/read-str (client/send! clj-http
+                                                      channel
+                                                      (json/write-str request))
+                                        :key-fn keyword)]
+            (is (= request (:json response)))
+            (is (= "application/json" (-> response :headers :content-type)))
+            (is (= "application/json" (-> response :headers :accept))))
+          (finally (client/close clj-http channel)))))))
--- core/test/json_rpc/ws_test.clj
+++ core/test/json_rpc/ws_test.clj
@@ -7,15 +7,17 @@
 
 (deftest ^:integration gniazdo-test
   (testing "with echo response"
-    (let [channel (client/open gniazdo "ws://echo.websocket.org")]
-      (try
-        (let [request    {:jsonrpc "2.0"
+    (doseq [url ["ws://echo.websocket.org"
+                 "wss://echo.websocket.org"]]
+      (let [channel (client/open gniazdo url)]
+        (try
+          (let [request  {:jsonrpc "2.0"
                           :method  "eth_blockNumber"
                           :params  ["latest"]
                           :id      1}
-              response   (json/read-str (->> request
+                response (json/read-str (->> request
                                              (json/write-str)
                                              (client/send! gniazdo channel))
                                         :key-fn keyword)]
-          (is (= request response)))
-        (finally (client/close gniazdo channel))))))
+            (is (= request response)))
+          (finally (client/close gniazdo channel)))))))
