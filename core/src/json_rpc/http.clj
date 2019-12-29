39fdd8312c8b341a60377fbef484b5d31ad2ba67
Hindol Adhya <hiadhy@microsoft.com>
2019-12-29 13:07:35 +0530

    Remove Pedestal interceptor and also drop the dependency

--- core/src/json_rpc/http.clj
+++ core/src/json_rpc/http.clj
@@ -3,6 +3,29 @@
    [clj-http.client :as http]
    [json-rpc.client :as client]))
 
+(defn- parse-int
+  [s]
+  (Integer/parseInt s))
+
+(defn infer-http-status
+  "Infer the HTTP status code from the JSON-RPC response.
+   See: https://www.jsonrpc.org/historical/json-rpc-over-http.html#errors"
+  [body]
+  (if-let [error (:error body)]
+    (let [code (:code error)]
+      (try
+        (let [code (parse-int code)]
+          (cond
+            (= -32600 code)                400
+            (= -32601 code)                404
+            (#{-32602 -32603 -32700} code) 500
+            (<= -32099 -32000)             500
+            :else (throw (ex-info "JSON-RPC error code invalid!"
+                                  {:response body}))))
+        (catch java.lang.NumberFormatException ex
+          (throw (ex-info "JSON-RPC error code is not a number!"
+                          {:response body} ex)))))))
+
 (defrecord CljHttpClient [options]
   client/Client
 
